f5433fd999abd5cfca86a148b2b8fb7a7497bb9a
David Son <sonkdav@gmail.com>
2019-07-12 17:45:02 -0700

    Added comments for readability.

--- similarsearch.py
+++ similarsearch.py
@@ -4,6 +4,7 @@
 # versions of that word.
 
 def main():
+    # Take in the 3 user inputs (file, field, word to search for)
     newFile = input("What is the file name? ")
     field = input("What is the field you are looking for? ")
     newText = input("What is the text you are looking for? ")
@@ -12,6 +13,7 @@ def main():
     lineCount = 0
     newList = []
 
+    # Open a file and parse through each document and add it to a list.
     with open(newFile, encoding="utf8") as thisFile:
         for line in thisFile:
             if fieldText in line:
@@ -19,6 +21,9 @@ def main():
                 lineCount += 1
 
     similarCount = {}
+
+    # Use some natural language processing to extract unique values and insert
+    # into a dictionary. if there are equivalent values, increase that specific counter.
     for i in newList:
         subStart = i.index(field)
         subEnd = i.index('\",\"', subStart)
@@ -35,6 +40,9 @@ def main():
     print("Results:")
     print("Line Count:", lineCount)
     print()
+
+    # Print out each dictionary item and how many times it was found/percentage of all
+    # documents with that word or phrase.
     for key,val in sorted(similarCount.items(), key = lambda kv:(kv[1], kv[0])):
         print(key, "=>", val, "|", (val/lineCount)*100,"%")
 
