26dfb500041e5741094a1c9d5e585478a89573eb
David Son <sonkdav@gmail.com>
2019-07-29 11:35:23 -0700

    MongoDB connection works!

--- similarsearch.py
+++ similarsearch.py
@@ -1,5 +1,5 @@
 # David Son 7/12/19
-# This program takes in a MongoDB .json export file, a field, and a word to search for
+# This program takes in a field, and a word to search for
 # and outputs all similar words or phrases and displays the top 10
 # words found within the related descriptions.
 
@@ -7,6 +7,7 @@ import decimal
 import nltk
 import string
 import re
+import json
 from pymongo import MongoClient
 from difflib import SequenceMatcher
 from nltk.stem import WordNetLemmatizer
@@ -14,86 +15,65 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 # Choose a function based on user input.
 def main():
+
     # Connecting to MongoDB for all of the documents.
     client = MongoClient('mongodb://34.73.180.107:27017')
     db = client.smartcareer
     col = db['jobdescription']
-    allDocs = list(col.find())
+    allDocs = col.find()
 
     # User input for search function
     field = string.capwords(input("What is the field you are looking for? "))
     newText = string.capwords(input("What is the text you are looking for? \nType 'All+' for all unique words of that topic.\n"))
     print()
-    fieldText = field + "\"" + ":" + "\"" + newText
 
-    lineCount = 0
-    newList = []
-    print(allDocs[0])
-
-    # Open a file and parse all lines and add to list.
-    if newText == "All+":
-        for document in allDocs:
-            newList.append(document)
-            lineCount += 1
-        varSearch(newList, lineCount, field)
-
-    # Open a file and parse through lines, only adding lines that have the key word/phrase included.
-    else: 
-        for document in allDocs:
-            if fieldText in document:
-                newList.append(document)
-                lineCount += 1
-        varSearch(newList, lineCount, field)
+    varSearch(allDocs, field, newText)
 
 # Take each document and find words or phrases similar to it.
 # If there are equivalent values, increase the counter for that specific word.
-def varSearch(newList, lineCount, field):
+def varSearch(docs, field, newText):
     similarCount = {}
     descCount = {}
     groupCount = {}
     temp4 = []
+    lineCount = 1
 
-    for i in newList:
+    for i in docs:
         try :
-            subStart = i.index('\"' + field + '\"')
-            subEnd = i.find('\",\"', subStart)
+            value = i.get(field)
+            if newText in value:
+                cleanDesc = i.get('Description')
 
-            if i[subEnd - 1] == "}":
-                compName = i[subStart:subEnd - 2]
-            else:
-                compName = i[subStart:subEnd]
+                if value in similarCount:
+                    similarCount[value] = similarCount.get(value) + 1
 
-            cStart = compName.index(':\"')
-            cleanName = compName[cStart + 2:]
+                else:
+                    similarCount[value] = 1
+                    if len(similarCount) > 0:
+                        match = SequenceMatcher(None, value, list(similarCount.keys())[-2]).find_longest_match(0, len(value), 0, len(list(similarCount.keys())[-2]))
 
-            if cleanName in similarCount:
-                similarCount[cleanName] = similarCount.get(cleanName) + 1
-            else:
-                similarCount[cleanName] = 1
-                if len(similarCount) > 0:
-                    match = SequenceMatcher(None, cleanName, list(similarCount.keys())[-2]).find_longest_match(0, len(cleanName), 0, len(list(similarCount.keys())[-2]))
+                        # Check the whole list for similar words and add to the group that is the shortest common subsequence of letters.
+                        for key in similarCount.keys():
+                            tMatch = SequenceMatcher(None, key, list(similarCount.keys())[-2]).find_longest_match(0, len(key), 0, len(list(similarCount.keys())[-2]))
+                            if tMatch[2] < match[2]:
+                                match = tMatch
 
-                    # Check the whole list for similar words and add to the group that is the shortest common subsequence of letters.
-                    for key in similarCount.keys():
-                        tMatch = SequenceMatcher(None, key, list(similarCount.keys())[-2]).find_longest_match(0, len(key), 0, len(list(similarCount.keys())[-2]))
-                        if tMatch[2] < match[2]:
-                            match = tMatch
+                        if match[2] > 5:
+                            pureName = punctuationRemover(value[match.a: match.a + match.size])
+                            pureName = pureName.strip()
 
-                    if match[2] > 5:
-                        pureName = punctuationRemover(cleanName[match.a: match.a + match.size])
-                        pureName = pureName.strip()
+                            if pureName in groupCount:
+                                groupCount[pureName] = groupCount[pureName] + 1
+                            else:
+                                groupCount[pureName] = 2
 
-                        if pureName in groupCount:
-                            groupCount[pureName] = groupCount[pureName] + 1
                         else:
-                            groupCount[pureName] = 2
-
-                    else:
-                        continue
+                            continue
+                
+                temp3, descCount = descriptionParser(cleanDesc, descCount)
+                temp4 = temp3 + temp4
+                lineCount += 1
             
-            temp3, cleanDesc = descriptionParser(i, descCount)
-            temp4 = temp3 + temp4
-        
         # Skip this document since the field does not exist for it.
         except:
             continue
@@ -105,18 +85,19 @@ def varSearch(newList, lineCount, field):
     # Print out each dictionary item and how many times it was found/percentage of all
     # documents with that word or phrase.
     for key,val in sorted(similarCount.items(), key = lambda kv:(kv[1], kv[0]), reverse = True):
-        print("Ratio: {:7s} Count: {:<5d} Text: {:40s}".format(str(round(((val/lineCount)*100),2))+"%",val, key))
+        print("Ratio: {:7s} Count: {:<5d} Text: {:40s}".format(str(round((val/lineCount*100),2))+"%",val, key))
     
     print()
     print("The group(s) of similar topics.\n")
+
     # Print out each topic and how many variations they have each.
     for key,val in sorted(groupCount.items(), key = lambda kv:(kv[1], kv[0]), reverse = True):
         print("Group: {:40s} Variations: {:1d}".format(key, val))
 
     print("\nThese are the top ten words for this topic:\n")
+    count = 1
 
     # Print the top 10 words in the descriptions of a certain job or topic.
-    count = 1
     for key,val in sorted(descCount.items(), key = lambda kv:(kv[1], kv[0]), reverse = True)[0:10]:
         print("{:3s} Word: {:20s} Count: {:4d}".format(str(count)+".","\""+key+"\"",val))
         count += 1
@@ -128,12 +109,7 @@ def varSearch(newList, lineCount, field):
     
 # Use some natural language processing to extract unique words
 # from the document's description and insert it into a dictionary.
-def descriptionParser(line, descCount):
-    descStart = line.index('\"Description\"')
-    descEnd = line.find('\",\"', descStart)
-    descName = line[descStart:descEnd]
-    descFull = descName.index(':\"')
-    cleanDesc = descName[descFull + 2:]
+def descriptionParser(cleanDesc, descCount):
     temp = punctuationRemover(cleanDesc)
     temp = re.sub(r'\d+', '', temp)
     splitDesc = tokenize(temp)
@@ -148,7 +124,7 @@ def descriptionParser(line, descCount):
         else :
             descCount[j] = 1
     
-    return temp3, cleanDesc
+    return temp3, descCount
 
 # NLP that removes all punctuation.
 def punctuationRemover(description):
@@ -166,6 +142,7 @@ def removeStop(splitDesc):
     returnText = [word for word in splitDesc if word not in stopword]
     return returnText
 
+# Gets only the root of each word (slower, but better than stemming).
 def lemmatizer(temp2):
     wn = WordNetLemmatizer()
     textReturn = []
