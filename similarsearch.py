a9db2fdd1d683535f4665a78207928d8a5ca0416
David Son <sonkdav@gmail.com>
2019-08-06 17:42:55 -0700

    Latest version 3.3

--- similarsearch.py
+++ similarsearch.py
@@ -1,8 +1,10 @@
 # David Son 7/12/19
 # This program takes in a field, and a word to search for
-# and outputs all similar words or phrases and displays the top 10
-# words found within the related descriptions.
+# and outputs all similar words or titles for that field.
+# Then it goes through all the documents and replaces
+# similar values with one standardizing group title.
 
+# Important packages.
 import decimal
 import nltk
 import string
@@ -13,21 +15,30 @@ import copy
 from pymongo import MongoClient
 from difflib import SequenceMatcher
 
-# Choose a function based on user input.
 def main():
 
-    # Connecting to MongoDB for all of the documents.
+    # Connecting to MongoDB to gather all of the documents.
     client = MongoClient('mongodb://34.73.180.107:27017')
     db = client.smartcareer
     col = db['jobdescription']
     allDocs = col.find({},no_cursor_timeout=True)
 
-    # User input for search function
+    # User input for search and replace function.
     field = string.capwords(input("What is the field you are trying to change? "))
     newText = string.capwords(input("Please enter the group name. \nType 'All+' for all unique words of that topic.\n"))
     print()
 
     varSearch(allDocs, field, newText)
+    
+# NLP that removes all punctuation.
+def punctuationRemover(description):
+    noPunctuation = "".join([char for char in description if char not in string.punctuation])
+    return noPunctuation
+
+# NLP that splits words by space.
+def tokenize(temp):
+    tokens = re.split(r'\W+', temp)
+    return tokens
 
 # Take each document and find words or phrases similar to it.
 # If there are equivalent values, increase the counter for that specific word.
@@ -37,6 +48,7 @@ def varSearch(docs, field, newText):
     groupCount = {}
     lineCount = 0
 
+    # Go through every document and for a specific field in each one, find similar groupings of words or titles.
     for i in docs:
         try:
             value = i.get(field)
@@ -66,6 +78,7 @@ def varSearch(docs, field, newText):
                 
                 lineCount += 1
             
+            # Special section for location only. Skips all the matching stuff since I already know what to replace it with.
             elif field == "Location":
                 if value in similarCount:
                     similarCount[value] = similarCount.get(value) + 1
@@ -82,54 +95,55 @@ def varSearch(docs, field, newText):
                             groupCount[pureName] = 1
                 
                 lineCount += 1
-        
-            elif newText == "All+":
 
-                if value in similarCount:
-                    similarCount[value] = similarCount.get(value) + 1
+            # UNDER CONSTRUCTION / Out of order
+            # elif newText == "All+":
 
-                else:
-                    similarCount[value] = 1
-                    if len(similarCount) > 0:
+            #     if value in similarCount:
+            #         similarCount[value] = similarCount.get(value) + 1
 
-                        # Check the whole list for similar words and add to the group that is the shortest common subsequence of letters.
-                        for key in similarCount.keys():
-                            tMatch = SequenceMatcher(None, value, key).find_longest_match(0, len(value), 0, len(key))
+            #     else:
+            #         similarCount[value] = 1
+            #         if len(similarCount) > 0:
 
-                        pureName = punctuationRemover(value[tMatch.a: tMatch.a + tMatch.size])
-                        pureName = pureName.strip()
+            #             # Check the whole list for similar words and add to the group that is the shortest common subsequence of letters.
+            #             for key in similarCount.keys():
+            #                 tMatch = SequenceMatcher(None, value, key).find_longest_match(0, len(value), 0, len(key))
+
+            #             pureName = punctuationRemover(value[tMatch.a: tMatch.a + tMatch.size])
+            #             pureName = pureName.strip()
                         
-                        # For All+, if there's more than one group we have to check through all of them for near-duplicates.
-                        if len(groupCount) > 0:
+            #             # For All+, if there's more than one group we have to check through all of them for near-duplicates.
+            #             if len(groupCount) > 0:
 
-                            # For each key in groupCount, try to get a match.
-                            for gKey in groupCount.keys():
-                                tMatch = SequenceMatcher(None, pureName, gKey).find_longest_match(0, len(pureName), 0, len(gKey))
+            #                 # For each key in groupCount, try to get a match.
+            #                 for gKey in groupCount.keys():
+            #                     tMatch = SequenceMatcher(None, pureName, gKey).find_longest_match(0, len(pureName), 0, len(gKey))
 
-                                # If they are exactly equal, add one to the counter of that key.
-                                if pureName == gKey:
-                                    groupCount[pureName] = groupCount[pureName] + 1
+            #                     # If they are exactly equal, add one to the counter of that key.
+            #                     if pureName == gKey:
+            #                         groupCount[pureName] = groupCount[pureName] + 1
 
-                                # Nearly matching, so it must be like 'Amazon' vs. 'Amazon Web Services'.
-                                elif tMatch[2] > 5:
+            #                     # Nearly matching, so it must be like 'Amazon' vs. 'Amazon Web Services'.
+            #                     elif tMatch[2] > 5:
 
-                                    # Take the shorter one and make that the main group. Remove the other longer key and give its value to the new one.
-                                    if len(pureName) < len(gKey):
-                                        addName = punctuationRemover(pureName[tMatch.a: tMatch.a + tMatch.size])
-                                        addName = addName.strip()
-                                        groupCount[addName] = groupCount.pop(gKey)
+            #                         # Take the shorter one and make that the main group. Remove the other longer key and give its value to the new one.
+            #                         if len(pureName) < len(gKey):
+            #                             addName = punctuationRemover(pureName[tMatch.a: tMatch.a + tMatch.size])
+            #                             addName = addName.strip()
+            #                             groupCount[addName] = groupCount.pop(gKey)
                                     
-                                    # Same as above.
-                                    else:
-                                        addName = punctuationRemover(gKey[tMatch.b: tMatch.b + tMatch.size])
-                                        addName = addName.strip()
-                                        groupCount[addName] = groupCount[addName] + 1
+            #                         # Same as above.
+            #                         else:
+            #                             addName = punctuationRemover(gKey[tMatch.b: tMatch.b + tMatch.size])
+            #                             addName = addName.strip()
+            #                             groupCount[addName] = groupCount[addName] + 1
                                
-                        else:
-                            groupCount[pureName] = 1
+            #             else:
+            #                 groupCount[pureName] = 1
 
-                    else:
-                        continue
+            #         else:
+            #             continue
 
                 lineCount += 1
             
@@ -141,6 +155,7 @@ def varSearch(docs, field, newText):
     print("Total Found:", lineCount,"\n")
     print("All similar texts:\n")
 
+    # Rare occassion that similarsearch does not find anything. No need to continue.
     if (len(groupCount) == 0):
         print("There were no matches! Now stopping the program.")
         exit()
@@ -158,20 +173,12 @@ def varSearch(docs, field, newText):
         print("Group: {:40s} Variations: {:1d}".format(key, val))
         print()
 
-    choice = input("All of the above (unless field is location) will be changed to", newText, "is that ok? (Yes or No):")
+    # This section is just to confirm what will be changed in the main collection.
+    choice = input("All of the above (unless field is location) will be changed to \"" + newText + "\" is that ok? (Yes or No):")
+
     if choice.upper() == "YES":
         docReplacer(doClone, field, groupCount)
     exit()
-    
-# NLP that removes all punctuation.
-def punctuationRemover(description):
-    noPunctuation = "".join([char for char in description if char not in string.punctuation])
-    return noPunctuation
-
-# NLP that splits words by space.
-def tokenize(temp):
-    tokens = re.split(r'\W+', temp)
-    return tokens
 
 # This method receives all MongoDB documents, the field, and groupCount which holds the standardized name
 # and re-uploads a clean standardized dataset to MongoDB.
